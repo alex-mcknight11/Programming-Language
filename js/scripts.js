8b821e2577495509bcfba64c8908266527c53bf1
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-14 12:06:08 -0800

    WIP form results

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,11 @@ const beach = $("input:radio[name=beach]:checked").val();
 const eggs = $("#eggs").val();
 
 const favoriteColor = $("#color").val();
+
+let cOption = 0;
+let javacriptOption = 0;
+let rubyOption = 0;
+let pythonOption = 0;
+let wrenOption = 0;
+
+if (result 'cOption' > 'javascriptOption', 'rubyOption', 'pythonOption', 'wrenOption')

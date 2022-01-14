5446414240c30044ed7d06b86415b9fb41a78ca7
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-14 11:05:33 -0800

    add final form options

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ const movie = $("input:radio[name=movie]:checked").val();
 const wordAssociation = $("#wordAssociation").val();
 
 const beach = $("input:radio[name=beach]:checked").val();
+
+const eggs = $("#eggs").val();
+
+const favoriteColor = $("#color").val();

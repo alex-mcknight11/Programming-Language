b10c22bb2dcbb16ccb9a9b4abb63981a96d348aa
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-23 21:43:36 -0800

    fix indentation and add 'else if' functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,127 +1,111 @@
-$(document).ready(function() {
-  $("#submitButton").submit(function(event) {
-    event.preventDefault();
+$(document).ready(function () {
+	$('#submitButton').submit(function (event) {
+		event.preventDefault();
 
-  function submitForm(){
-    $('submitButton')
-  }
+		function submitForm() {
+			$('submitButton');
+		}
 
-  const movie = $("input:radio[name=movie]:checked").val();
+		const movie = $('input:radio[name=movie]:checked').val();
 
-  const wordAssociation = $("#wordAssociation").val();
+		const wordAssociation = $('#wordAssociation').val();
 
-  const beach = $("input:radio[name=beach]:checked").val();
+		const beach = $('input:radio[name=beach]:checked').val();
 
-  const eggs = $("#eggs").val();
+		const eggs = $('#eggs').val();
 
-  const favoriteColor = $("#color").val();
-  const colorResult = $("#colorResult").text(favoriteColor);
+		const favoriteColor = $('#color').val();
+		const colorResult = $('#colorResult').text(favoriteColor);
 
-  let cSum = 0;
-  let javascriptSum = 0;
-  let rubySum = 0;
-  let pythonSum = 0;
-  let wrenSum = 0;
+		let cSum = 0;
+		let javascriptSum = 0;
+		let rubySum = 0;
+		let pythonSum = 0;
+		let wrenSum = 0;
 
-  if (movie === "cOption") {
-    cSum = cSum + 1;
-  }
+		if (movie === 'cOption') {
+			cSum = cSum + 1;
+		} else if (movie === 'javascriptOption') {
+			javascriptSum = javascriptSum + 1;
+		} else if (movie === 'rubyOption') {
+			rubySum = rubySum + 1;
+		} else if (movie === 'pythonOption') {
+			pythonSum = pythonSum + 1;
+		} else if (movie === 'wrenOption') {
+			wrenSum = wrenSum + 1;
+		}
 
-  if (movie === "javascriptOption") {
-    javascriptSum = javascriptSum + 1;
-  }
+		if (wordAssociation === 'cOption') {
+			cSum = cSum + 1;
+		} else if (wordAssociation === 'javascriptOption') {
+			javascriptSum = javascriptSum + 1;
+		} else if (wordAssociation === 'rubyOption') {
+			rubySum = rubySum + 1;
+		} else if (wordAssociation === 'pythonOption') {
+			pythonSum = pythonSum + 1;
+		} else if (wordAssociation === 'wrenOption') {
+			wrenSum = wrenSum + 1;
+		}
 
-  if (movie === "rubyOption") {
-    rubySum = rubySum + 1;
-  }
+		if (beach === 'cOption') {
+			cSum = cSum + 1;
+		} else if (beach === 'javascriptOption') {
+			javascriptSum = javascriptSum + 1;
+		} else if (beach === 'rubyOption') {
+			rubySum = rubySum + 1;
+		} else if (beach === 'pythonOption') {
+			pythonSum = pythonSum + 1;
+		} else if (beach === 'wrenOption') {
+			wrenSum = wrenSum + 1;
+		} else if (beach === 'cOption') {
+			cSum = cSum + 1;
+		}
 
-  if (movie === "pythonOption") {
-    pythonSum = pythonSum + 1;
-  }
+		if (eggs === 'javascriptOption') {
+			javascriptSum = javascriptSum + 1;
+		} else if (eggs === 'rubyOption') {
+			rubySum = rubySum + 1;
+		} else if (eggs === 'pythonOption') {
+			pythonSum = pythonSum + 1;
+		} else if (eggs === 'wrenOption') {
+			wrenSum = wrenSum + 1;
+		}
 
-  if (movie === "wrenOption") {
-    wrenSum = wrenSum + 1;
-  }
-
-  if (wordAssociation === "cOption") {
-    cSum = cSum + 1;
-  }
-
-  if (wordAssociation === "javascriptOption") {
-    javascriptSum = javascriptSum + 1;
-  }
-
-  if (wordAssociation === "rubyOption") {
-    rubySum = rubySum + 1;
-  }
-
-  if (wordAssociation === "pythonOption") {
-    pythonSum = pythonSum + 1;
-  }
-
-  if (wordAssociation === "wrenOption") {
-    wrenSum = wrenSum + 1;
-  }
-
-  if (beach === "cOption") {
-    cSum = cSum + 1;
-  }
-
-  if (beach === "javascriptOption") {
-    javascriptSum = javascriptSum + 1;
-  }
-
-  if (beach === "rubyOption") {
-    rubySum = rubySum + 1;
-  }
-
-  if (beach === "pythonOption") {
-    pythonSum = pythonSum + 1;
-  }
-
-  if (beach === "wrenOption") {
-    wrenSum = wrenSum + 1;
-  }
-
-  if (beach === "cOption") {
-    cSum = cSum + 1;
-  }
-
-  if (eggs === "javascriptOption") {
-    javascriptSum = javascriptSum + 1;
-  }
-
-  if (eggs === "rubyOption") {
-    rubySum = rubySum + 1;
-  }
-
-  if (eggs === "pythonOption") {
-    pythonSum = pythonSum + 1;
-  }
-
-  if (eggs === "wrenOption") {
-    wrenSum = wrenSum + 1;
-  }
-
-  if (cSum >= javascriptSum && cSum >= rubySum && cSum >= pythonSum && cSum >= wrenSum) {
-    $('#cResult').show();
-  }
-
-  else if (javascriptSum >= cSum && javascriptSum >= rubySum && javascriptSum >= pythonSum && javascriptSum >= wrenSum) {
-    $('#javascriptResult').show();
-  }
-
-  else if (rubySum >= cSum && rubySum >= javascriptSum && rubySum >= pythonSum && rubySum >= wrenSum) {
-    $('#rubyResult').show();
-  }
-
-  else if (pythonSum >= cSum && pythonSum >= javascriptSum && pythonSum >= rubySum && pythonSum >= wrenSum) {
-    $('#pythonResult').show();
-  }
-
-  else if (wrenSum >= cSum && wrenSum >= javascriptSum && wrenSum >= rubySum && wrenSum >= pythonSum) {
-    $('#wrenResult').show();
-  }
-
-  });
+		if (
+			cSum >= javascriptSum &&
+			cSum >= rubySum &&
+			cSum >= pythonSum &&
+			cSum >= wrenSum
+		) {
+			$('#cResult').show();
+		} else if (
+			javascriptSum >= cSum &&
+			javascriptSum >= rubySum &&
+			javascriptSum >= pythonSum &&
+			javascriptSum >= wrenSum
+		) {
+			$('#javascriptResult').show();
+		} else if (
+			rubySum >= cSum &&
+			rubySum >= javascriptSum &&
+			rubySum >= pythonSum &&
+			rubySum >= wrenSum
+		) {
+			$('#rubyResult').show();
+		} else if (
+			pythonSum >= cSum &&
+			pythonSum >= javascriptSum &&
+			pythonSum >= rubySum &&
+			pythonSum >= wrenSum
+		) {
+			$('#pythonResult').show();
+		} else if (
+			wrenSum >= cSum &&
+			wrenSum >= javascriptSum &&
+			wrenSum >= rubySum &&
+			wrenSum >= pythonSum
+		) {
+			$('#wrenResult').show();
+		}
+	});
 });
